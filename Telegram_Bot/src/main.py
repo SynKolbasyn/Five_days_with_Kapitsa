0d1360826389f8c2c6b01f063633c714bea41080
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2023-09-16 15:41:07 +0300

    feat(TG_Bot): Added the logic of the bot

--- Telegram_Bot/src/main.py
+++ Telegram_Bot/src/main.py
@@ -1,38 +1,79 @@
+import os
 import asyncio
 import logging
-import sys
 
-from aiogram import Bot, Dispatcher, types
-from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-from aiogram.utils.markdown import hbold
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, FSInputFile, BotCommand
+
+from functions import message_processing
 
 
-with open("../../game_data/token.txt", "r") as file:
-    TOKEN = file.read()
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(message)s",
+    handlers=[
+        logging.FileHandler(filename="../game_data/logs/telegram_logs.log", mode="a"),
+        logging.StreamHandler()
+    ]
+)
+
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(TOKEN)
 
 dp = Dispatcher()
 
 
+async def setup_bot_commands(dispatcher):
+    bot_commands = [
+        BotCommand(command="/start", description="Show start menu"),
+        BotCommand(command="/help", description="Show help menu")
+    ]
+    await bot.set_my_commands(bot_commands)
+
+
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+    """
+    Tells the player about this bot: what it can do and what it is needed for
+
+    :param message: message from player
+    :return: None
+    """
+
+    await message.reply_photo(
+        FSInputFile("../resources/images/start.png"),
+        "A message that tells the player about this bot: what it can do and what it is needed for"
+    )
+
+
+@dp.message(Command("help"))
+async def command_help_handler(message: Message) -> None:
+    """
+    Tells the player how to use the bot
+
+    :param message: message from player
+    :return: None
+    """
+
+    await message.reply_photo(
+        FSInputFile("../resources/images/help.png"),
+        "A message that tells the player how to use the bot"
+    )
 
 
 @dp.message()
-async def echo_handler(message: types.Message) -> None:
-    try:
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        await message.answer("Nice try!")
+async def main_handler(message: Message) -> None:
+    answer, image = await message_processing(message, bot)
+    await message.reply_photo(image, answer)
 
 
 async def main() -> None:
-    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    await dp.startup.register(setup_bot_commands)
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
